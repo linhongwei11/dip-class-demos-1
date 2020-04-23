fd8186a0659e2d2c9e60333613b7f7922c17965d
lizhu <lizhu@DESKTOP-8KGBRMQ>
2020-04-23 14:31:39 +0800

    创建09分支

--- dip-class-demos/funtions.cpp
+++ dip-class-demos/funtions.cpp
@@ -1,4 +1,22 @@
 #include "stdafx.h"
 #include "funtions.h"
+#include <opencv.hpp>
 
 
+
+using namespace cv;
+
+int detectPeople()
+{
+
+	//VideoCapture cap(../testImages);
+
+	HOGDescriptor hog;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+
+
+
+
+	return 0;
+}
