d8988c596fb771cd7d381cd7a047cdb03534b502
lizhu <lizhu@DESKTOP-8KGBRMQ>
2020-05-13 17:35:32 +0800

    增加基本绘图demo分支

--- dip-class-demos/dip-class-demos.cpp
+++ dip-class-demos/dip-class-demos.cpp
@@ -13,6 +13,20 @@ int main()
 	//开始计时
 	double start = static_cast<double>(cvGetTickCount());
 
+	//基本图形的绘制
+
+	//绘制直线
+	//drawLines();
+
+	//绘制矩形
+	//drawRectangles();
+
+	//绘制圆形
+	//drawCircles();
+
+	//绘制椭圆，或圆弧
+	//drawEllipse();
+
 
 	//结束计时
 	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
