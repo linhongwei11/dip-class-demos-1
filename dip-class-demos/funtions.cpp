01e3d606c6dd05b01f980aa7a876917ebb276687
Zhu Li <crazyjoe874@msn.com>
2020-05-14 21:52:04 +0800

    增加直方图增强分支

--- dip-class-demos/funtions.cpp
+++ dip-class-demos/funtions.cpp
@@ -1,4 +1,36 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "funtions.h"
 
 
+
+int gammaMain()
+{
+
+
+
+	return 0;
+}
+
+int gammaCorrection(cv::Mat srcMat, cv::Mat & dstMat, float gamma)
+{
+
+	//本函数只处理单通道图像
+	if (srcMat.channels!=1)return -1;
+
+	//建立查询表
+	unsigned char lut[256];
+	for (int i = 0; i < 256; i++)
+	{
+		//saturate_cast，防止像素值溢出，如果值<0,则返回0，如果大于255，则返回255
+		lut[i] = saturate_cast<uchar>(pow((float)(i / 255.0), gamma) * 255.0f);
+	}
+
+	srcMat.copyTo(dstMat);
+
+	MatIterator_<uchar> it, end;
+	for (it = dstMat.begin<uchar>(), end = dstMat.end<uchar>(); it != end; it++) {
+		*it = lut[(*it)];
+	}
+
+	return 0;
+}
--- dip-class-demos/funtions.h
+++ dip-class-demos/funtions.h
@@ -1,2 +1,16 @@
 #pragma once
+#include <opencv.hpp>
+
+
+using namespace cv;
+
+
+//gamma矫正演示函数
+int gammaMain();
+
+
+
+//gamma矫正功能函数
+int gammaCorrection(cv::Mat srcMat, cv::Mat & dstMat, float gamma);
+
 
